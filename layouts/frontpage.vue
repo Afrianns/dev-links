31dea8c4c214682ae7aa74cbe551c7658774eef3
HanifNAfrian <hanifnafrian12@gmail.com>
2025-02-18 07:08:54 +0700

    update + fixed login/register layout

--- layouts/frontpage.vue
+++ layouts/frontpage.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="flex flex-col items-center w-[500px] md:w-full justify-center h-screen">
+    <div class="flex md:flex-col items-center w-full justify-center h-screen">
         <div
-            class="w-full md:w-[820px] md:h-[500px] bg-white md:shadow m-auto rounded-md md:border md:border-gray-50 overflow-hidden">
+            class="w-full max-md:max-w-[500px] md:w-[820px] md:h-[500px] bg-white md:shadow m-auto rounded-md md:border md:border-gray-50 overflow-hidden">
             <div class="flex md:h-full">
                 <section
                     class="md:block hidden w-7/12 bg-cover bg-center bg-[url(assets/images/person-with-computer.jpg)] bg-no-repeat bg-orange-100">
